1b1f8d44c5b0bcd19d227b6047350552eb1b5846
chelahcynthia <cchelangat989@gmail.com>
2023-10-18 08:44:17 +0300

    Added styles

--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import './Coins.css'
+// import CoinItem from './CoinItem'
+// import Coin from '../routes/Coin'
+// import { Link } from 'react-router-dom'
 
-function Coins() {
+const Coins = (props) => {
   return (
-    <div>Coins</div>
+    <div className='container'>
+      <div>
+        <div className='heading'>
+          <p>#</p>
+          <p className='coin-name'>Coin</p>
+          <p>Price</p>
+          <p>24h</p>
+          <p className='hide-mobile'>Volume</p>
+          <p className='hide-mobile'>Mkt Cap</p>
+          </div>
+
+          {/* {props.coins.map(coins => {
+                    return (
+                        <Link to={`/coin/${coins.id}`} element={<Coin />} key={coins.id}>
+                            <CoinItem coins={coins} />
+                        </Link>
+
+                    )
+                })} */}
+        
+      </div>
+      
+      </div>
   )
 }
 
